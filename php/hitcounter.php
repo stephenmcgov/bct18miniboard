67143e5cee805f75a06f8d1c093bf9ad48cae2f9
smcgov-nuigalway <stephenmcgov@hotmail.co.uk>
2018-03-22 23:06:02 +0000

    Update hitcounter.php

--- php/hitcounter.php
+++ php/hitcounter.php
@@ -1,4 +1,5 @@
 <?php
+#open hitcounter txt file and print value
 $count_my_page = ("php/hitcounter.txt");
 $fp = fopen($count_my_page , "r");
 $hits = intval(trim(fread($fp, filesize($count_my_page))));
